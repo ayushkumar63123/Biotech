efc7b0dc483f8cf6d571eab9bf2eed087150b40d
ayushkumar63123 <86553805+ayushkumar63123@users.noreply.github.com>
2021-07-04 17:23:46 +0530

    Update submission1.py

--- submission1.py
+++ submission1.py
@@ -11,55 +11,6 @@ def speak(filename, text):
 
 speak('Hello! Welcome to this biotech app!', '111')
 
-speak("Please enter 1 if you want to find genes in a dna sequence. Enter 2 to find air quality health index", '114')
-
-command1 = input("Please enter 1 if you want to find first gene in a dna sequence. Enter 2 to find air quality health index")
-
-
-def find_gene(filename):
-    with open(filename + '.txt', 'r') as file1:
-        dna = file1.read()
-        if 'ATG' not in dna:
-            print('No gene found. System exiting....')
-            print("Press any key to exit....")
-            input()
-            sys.exit()
-        else:
-            codon_start = dna.index('ATG')
-
-        if 'TAA' in dna:
-            if 'TAG' in dna and dna.index('TAG', codon_start + 3) < dna.index('TAA', codon_start + 3):
-                if 'TGA' in dna and dna.index('TGA', codon_start + 3) < dna.index('TAG', codon_start + 3):
-                    stop_codon = dna.index('TGA')
-                else:
-                    stop_codon = dna.index('TAG')
-            elif 'TGA' in dna and dna.index('TGA', codon_start + 3) < dna.index('TAA', codon_start + 3):
-                if 'TAG' in dna and dna.index('TAG', codon_start + 3) < dna.index('TGA', codon_start + 3):
-                    stop_codon = dna.index('TAG')
-                else:
-                    stop_codon = dna.index('TGA')
-            else:
-                stop_codon = dna.index('TAA')
-        elif 'TGA' in dna:
-            if 'TAG' in dna and dna.index('TAG') < dna.index('TGA'):
-                stop_codon = dna.index('TAG')
-            else:
-                stop_codon = dna.index('TGA')
-        elif 'TAG' in dna:
-            if 'TGA' in dna and dna.index('TGA') < dna.index('TAG'):
-                stop_codon = dna.index('TGA')
-            else:
-                stop_codon = dna.index('TAG')
-        else:
-            print('No gene found. System exiting....')
-            print('Enter any key to exit....')
-            input()
-            sys.exit()
-
-        gene = dna[start_codon:stop_codon]
-
-        print('The following is the first gene found: ', '\n', gene)
-
 def airqualityindex(o3, no2, pm):
     import requests
 
@@ -76,14 +27,8 @@ def airqualityindex(o3, no2, pm):
 
     print(response.text)
 
-if command1 == '1':
-    speak('Please enter the .txt filename containing the dna sequence only', '112')
-    dna_file = input("Please enter the .txt filename containing the dna sequence only: ")
-    find_gene(dna_file)
-
-if command1 == '2':
-    o3 = int(input("Enter the ppm quantity of o3: "))
-    no2 = int(input("Enter the ppm quantity of no2: "))
-    pm = int(input("Enter the ppm quantity of pm: "))
-    print('Air Quality Health Index:-')
-    airqualityindex(o3, no2, pm)
+o3 = int(input("Enter the ppm quantity of o3: "))
+no2 = int(input("Enter the ppm quantity of no2: "))
+pm = int(input("Enter the ppm quantity of pm: "))
+print('Air Quality Health Index:-')
+airqualityindex(o3, no2, pm)
